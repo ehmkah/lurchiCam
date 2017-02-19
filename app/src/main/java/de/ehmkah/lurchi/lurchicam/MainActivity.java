ede3dd82a8ddf7d9afbf9d19486e455161c9f325
Michael Krausse <michael.krausse@directbox.com>
2017-02-19 14:32:41 +0100

    simple use case which makes a photo after a button has been klicked

--- app/src/main/java/de/ehmkah/lurchi/lurchicam/MainActivity.java
+++ app/src/main/java/de/ehmkah/lurchi/lurchicam/MainActivity.java
@@ -1,53 +1,129 @@
 package de.ehmkah.lurchi.lurchicam;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    private Date lastDate;
     private final static long WARTEZEIT_IN_MILLISEKUNDEN = 1000L;
+
+    private boolean hasCamera;
+    private Camera mCamera;
+    private CameraPreview mCameraPreview;
+
+    private Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            File pictureFile = getOutputMediaFile();
+            if (pictureFile == null) {
+                Log.d("TAG", "error creating media type.");
+                return;
+            }
+
+            try {
+                FileOutputStream outputStream = new FileOutputStream(pictureFile);
+                outputStream.write(data);
+                outputStream.close();
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private File getOutputMediaFile() {
+            File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "LurchiCam");
+
+            if (!mediaStorageDir.exists()) {
+                mediaStorageDir.mkdir();
+            }
+
+            String timeStamp = new SimpleDateFormat("yyyyMMDD_HHmmss").format(new Date());
+            File mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+
+            return mediaFile;
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        lastDate = new Date();
         setContentView(R.layout.activity_main);
+        hasCamera = checkCameraHardware(getApplicationContext());
+        if (hasCamera) {
+            mCamera = getCameraInstance();
+            // mCameraPreview = new CameraPreview(this, mCamera);
+            // FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+            //  preview.addView(mCameraPreview);
+        }
+
+    }
+
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            return true;
+        }
+
+        return false;
     }
 
     public void klick(View v) {
         System.out.println("HUHU");
+        mCamera.startPreview();
+        mCamera.takePicture(null, null, mPictureCallback);
+    }
+
+    public static Camera getCameraInstance() {
+        Camera result = null;
+        try {
+            result = Camera.open();
+        } catch (Exception e) {
+            // should not happen
+            System.out.println(e);
+        }
+        return result;
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        if (mCamera == null) {
+            mCamera = getCameraInstance();
+        }
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        mCamera.release();
+        mCamera = null;
+
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (new Date().getTime() - WARTEZEIT_IN_MILLISEKUNDEN >= lastDate.getTime()) {
-            String value = String.format("%s, Temperatur: %s°C", new Date(), event.values[0]);
-            System.out.println(value);
-            lastDate = new Date();
-        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
     }
+
+
 }
